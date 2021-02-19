669aebe2e0f23f86addf016ae0ce0ccdad594ed9
Pranali1299 <pranalipatil3620@gmail.com>
2021-02-19 14:43:31 +0530

    Add Operation file Added

---
+++ ArithmeticOperations.java
@@ -0,0 +1,12 @@
+class AirthmeticOperations {
+	
+	public void add(int a, int b){
+		
+		System.out.print(a+b);
+	}
+	public static void main(String[] args){
+	
+		AirthmeticOperations a = new AirthmeticOperations();
+		a.add(10,20);	
+	}
+}
