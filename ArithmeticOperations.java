99b9b11ee4aa53ffe56740b6a0dcac487595578a
Pranali1299 <pranalipatil3620@gmail.com>
2021-02-21 21:20:09 +0530

    mul method added

--- ArithmeticOperations.java
+++ ArithmeticOperations.java
@@ -4,9 +4,16 @@ class AirthmeticOperations {
 		
 		System.out.print(a+b);
 	}
+
+	public void mul(int a, int b){
+		
+		System.out.print(a*b);
+	}
+
 	public static void main(String[] args){
 	
 		AirthmeticOperations a = new AirthmeticOperations();
 		a.add(10,20);	
+		a.mul(10,20);
 	}
 }
