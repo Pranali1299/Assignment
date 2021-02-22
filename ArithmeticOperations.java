491144cc7b67da2b4731989792cedabc433f7eef
Pranali1299 <pranalipatil3620@gmail.com>
2021-02-22 17:55:00 +0530

    Revert "Revert "Revert "mul function removed"""
    This reverts commit 7c88fa7aea84e5df04a0b2b3d89a188441e803b9.

--- ArithmeticOperations.java
+++ ArithmeticOperations.java
@@ -5,29 +5,20 @@ class AirthmeticOperations {
 		System.out.print(a+b);
 	}
 
-
-	public void mul(int a, int b){
-		
-		System.out.print(a*b);
-
 	public void sub(int a, int b){
 		
-
 		int c = a-b ;
 		if(c<0){
 			c=-c;
 		}
 		System.out.print(c);
-
 	}
 	
 
 	public static void main(String[] args){
 	
 		AirthmeticOperations a = new AirthmeticOperations();
-
-		a.add(10,20);	
-		a.mul(10,20);
+		a.add(10,20);
 		a.sub(10,20);
 	}
 }
